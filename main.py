40d8b1d87188b4412d7d37883cb4dd99451d9432
ChiIIerr <wilsontruitt@gmail.com>
2024-02-27 01:24:08 -0600

    Update main.py

--- main.py
+++ main.py
@@ -15,12 +15,10 @@ concurrent_calls = 50
 # Semaphore for controlling concurrent API calls
 semaphore = asyncio.Semaphore(concurrent_calls)
 
-# Retry pool for failed activities
-retry_pool = []
-
-async def call_api(api_key, activity_id, semaphore, retry_pool):
+async def call_api(api_key, activity_id, semaphore):
     headers = {
-        'X-API-Key': api_key
+        'X-API-Key': api_key,
+        'Cache-Control': 'no-cache'  # Add cache control header
     }
     url = f'https://www.bungie.net/Platform/Destiny2/Stats/PostGameCarnageReport/{activity_id}/'
     async with semaphore:
@@ -29,25 +27,34 @@ async def call_api(api_key, activity_id, semaphore, retry_pool):
                 try:
                     async with session.get(url, headers=headers) as response:
                         if response.status == 200:
+                            if 'ThrottleSeconds' in response.headers and int(response.headers['ThrottleSeconds']) > 0:
+                                print(f"ThrottleSeconds is greater than 0 for activity: {activity_id}")
+                            else:
+                                continue
                             return await response.json()
                         else:
-                            retry_pool.append(activity_id)
-                            return None
+                            print(f"API call failed for activity: {activity_id}. Retrying in 10 seconds...")
+                            await asyncio.sleep(10)
+                            continue
+                            # Check if the response header has a cache hit
+                            if cache_status and 'public' in cache_status:
+                                print(f"Cache hit for activity: {activity_id}")
                 except aiohttp.ClientConnectionError:
                     print(f"Server disconnected. Retrying in 5 seconds...")
                     time.sleep(5)
                 except asyncio.TimeoutError:
                     print(f"Timeout error occurred for activity: {activity_id}")
-                    retry_pool.append(activity_id)
                     return None
 
-async def process_activity(api_key, activity_id, semaphore, conn, retry_pool):
-    json_data = await call_api(api_key, activity_id, semaphore, retry_pool)
+async def process_activity(api_key, activity_id, semaphore, conn):
+    json_data = await call_api(api_key, activity_id, semaphore)
     if json_data is not None:
         activity_details = json_data['Response']['activityDetails']
         if activity_details['mode'] == 84:
             print(f"Found activity with mode 84: {activity_id}")
             insert_data(conn, activity_id, json_data)
+        # else:
+        # print(f"Not trials, but ID is {activity_id}")
 
 def create_schema(conn):
     conn.execute('''CREATE TABLE IF NOT EXISTS activity
@@ -97,15 +104,16 @@ def create_schema(conn):
                       standing INTEGER NOT NULL,
                       UNIQUE(team_id, activity),
                       FOREIGN KEY (activity) REFERENCES activity (activity_id) ON DELETE CASCADE)''')
-    conn.execute('''CREATE TABLE IF NOT EXISTS weapon_result
+    conn.execute('''CREATE TABLE IF NOT EXISTS weapons
                      (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-                      reference_id INTEGER NOT NULL,
+                      activity INTEGER NOT NULL,
+                      FOREIGN KEY (activity) REFERENCES activity (activity_id) ON DELETE CASCADE),
+                      weapon_reference_id INTEGER NOT NULL,
                       kills INTEGER NOT NULL,
                       precision_kills INTEGER NOT NULL,
                       kills_precision_kills_ratio REAL NOT NULL,
-                      character_activity_stats INTEGER NOT NULL,
-                      UNIQUE(character_activity_stats, reference_id),
-                      FOREIGN KEY (character_activity_stats) REFERENCES character_activity_stats (id) ON DELETE CASCADE)''')
+                      character INTEGER NOT NULL,
+                      FOREIGN KEY (character) REFERENCES character_activity_stats (character) ON DELETE CASCADE)''')
 
 def insert_data(conn, activity_id, json_data):
     activity_details = json_data['Response']['activityDetails']
@@ -125,29 +133,35 @@ def insert_data(conn, activity_id, json_data):
 
     for entry in json_data['Response']['entries']:
         player = entry['player']
-        
+
         # Insert character activity stats into the table
         character = entry['characterId']
-        weapon_kills_super = entry['extended']['values']['weaponKillsSuper']['basic']['value']
+        weapon_kills_super = entry['extended']['values']['weaponKillsSuper']['basic']['value'] if 'extended' in entry else None
         kills = entry['values']['kills']['basic']['value']
         deaths = entry['values']['deaths']['basic']['value']
         opponents_defeated = entry['values']['opponentsDefeated']['basic']['value']
         time_played_seconds = entry['values']['timePlayedSeconds']['basic']['value']
-        
+
         conn.execute("INSERT INTO character_activity_stats (activity, character, score, kills, deaths, completed, opponents_defeated, standing, team, time_played_seconds, team_score, weapon_kills_super) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (activity_id, character, 0, kills, deaths, 0, opponents_defeated, 0, 0, time_played_seconds, 0, weapon_kills_super))
         
-    conn.commit()   
-    
+    conn.commit()  
 
-async def retry_activities(api_key, retry_pool, semaphore, conn):
-    while True:
-        await asyncio.sleep(60)  # Retry every 1 minute
-        print("Retrying activities...")
-        tasks = []
-        for activity_id in retry_pool:
-            tasks.append(process_activity(api_key, activity_id, semaphore, conn, retry_pool))
-        retry_pool.clear()
-        await asyncio.gather(*tasks)
+    for entry in json_data['Response']['entries']:
+        player = entry['player']
+
+        # Insert character activity stats into the table
+        character = entry['characterId']
+        weapon_kills_super = entry['extended']['values']['weaponKillsSuper']['basic']['value'] if 'extended' in entry else None
+        kills = entry['values']['kills']['basic']['value']
+        deaths = entry['values']['deaths']['basic']['value']
+        opponents_defeated = entry['values']['opponentsDefeated']['basic']['value']
+        time_played_seconds = entry['values']['timePlayedSeconds']['basic']['value']
+
+        conn.execute("INSERT INTO character_activity_stats (activity, character, score, kills, deaths, completed, opponents_defeated, standing, team, time_played_seconds, team_score, weapon_kills_super) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (activity_id, character, 0, kills, deaths, 0, opponents_defeated, 0, 0, time_played_seconds, 0, weapon_kills_super))
+        
+    conn.commit()  
+    
+     
 
 async def main():
     conn = sqlite3.connect(DATABASE_PATH)
@@ -155,12 +169,10 @@ async def main():
 
     activity_id = int(input("Enter the first activity ID: "))  # Move the variable assignment here
 
-    asyncio.create_task(retry_activities(api_key, retry_pool, semaphore, conn))
-
     while True:
         tasks = []
         for _ in range(concurrent_calls):
-            tasks.append(process_activity(api_key, activity_id, semaphore, conn, retry_pool))
+            tasks.append(process_activity(api_key, activity_id, semaphore, conn))
             activity_id += 1
         await asyncio.gather(*tasks)
 
